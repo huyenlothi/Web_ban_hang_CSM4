4cb0d6073ed12eb20b2b81bf024872e6d740aea4
huyenlothi <69282948+huyenlothi@users.noreply.github.com>
2020-12-07 15:09:32 +0700

    add file

--- src/main/java/com/example/demo/model/AppRole.java
+++ src/main/java/com/example/demo/model/AppRole.java
@@ -1,4 +1,22 @@
 package com.example.demo.model;
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "roles")
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String name;
+
 }
--- src/main/java/com/example/demo/model/AppUser.java
+++ src/main/java/com/example/demo/model/AppUser.java
@@ -1,4 +1,71 @@
 package com.example.demo.model;
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @NotNull
+    @Column(unique = true)
+    @Min(6)
+    private String name;
+    @Email
+    private String email;
+    @Min(6)
+    private String password;
+    @NotNull
+    private String phone;
+    @NotNull
+    private String address;
+
+    @Column(columnDefinition = "bit default true")
+    private boolean status;
+
+    @ManyToOne
+    @JoinColumn(name = "roles_id")
+    private AppRole role;
+
+    public AppRole getRole() {
+        return role;
+    }
+
+    public void setRole(AppRole role) {
+        this.role = role;
+    }
+
+    public AppUser(Long id, @Min(6) String name, @Email String email, @Min(6) String password, String phone, String address, boolean status, AppRole role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.address = address;
+        this.status = status;
+        this.role = role;
+    }
+
+    public AppUser(@Min(6) String name, @Email String email, @Min(6) String password, String phone, String address, boolean status, AppRole role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.address = address;
+        this.status = status;
+        this.role = role;
+    }
 }
